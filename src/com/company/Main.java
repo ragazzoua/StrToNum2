d47b7588ef6d640f77e8e3144a2dfc4db61be3ff
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-25 14:04:00 +0300

    StrToNum2

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(Integer.toBinaryString(262));
+        System.out.println(Integer.toHexString(262));
+        System.out.println(Integer.toOctalString(262));
+        System.out.println(Integer.toString(262));
+    }
+}
